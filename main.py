68cb9aae1f383a3a1e350cb32de2635f5e26c071
Rohit Suratekar <rohitsuratekar@gmail.com>
2020-03-01 11:35:29 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2020 , AnalyseSTRING
+#  Author: Rohit Suratekar
+#  Organisation: IIMCB, Warsaw
+#  Contact: rsuratekar [at] iimcb [dot] gov [dot] pl
+#
+# Simple scripts to analyse the STRING database (https://string-db.org/)
